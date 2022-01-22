f92555b143da24ab50298cd46cf9614296b69f07
Will Averill <wsaverill@gmail.com>
2022-01-21 23:27:32 -0800

    Implement user log in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDIpWrQcdN3sk5gnwSmvraY9ygdX6RQR3c",
+  authDomain: "would-you-rather-by-awesumtek.firebaseapp.com",
+  projectId: "would-you-rather-by-awesumtek",
+  storageBucket: "would-you-rather-by-awesumtek.appspot.com",
+  messagingSenderId: "431165189082",
+  appId: "1:431165189082:web:667e3e88e199be7c2eaee3",
+  measurementId: "G-N3G1FT3FL1",
+};
+
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+// Reference:  https://www.npmjs.com/package/react-firebaseui  Accessed:  1/21/22
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+
+  // Listen to the Firebase Auth state and set the local state.
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        setIsSignedIn(!!user);
+      });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <h1>Would You Rather?</h1>
+        <p>Please sign-in:</p>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Would You Rather?</h1>
+      <p>
+        Welcome {firebase.auth().currentUser.displayName}! You are now
+        signed-in!
+      </p>
+      <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
     </div>
   );
 }
