26cba42d7b3ca13c1c4b00fb133fb598890ca1a0
Will Averill <wsaverill@gmail.com>
2022-01-22 20:17:46 -0800

    Update middleware

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,3 +1,4 @@
+// Source: https://github.com/udacity/reactnd-redux-todos-goals/blob/1177c07de724399ba9f8b1573c4c9711caa23daa/src/middleware/logger.js Accessed: 1/22/22
 const logger = (store) => (next) => (action) => {
   console.group(action.type);
   console.log("The action: ", action);
