3f1c50f9a005a59577b041a2d709bfe796f0b7d0
Will Averill <wsaverill@gmail.com>
2022-01-22 23:36:47 -0800

    Update index

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import { handleInitialData } from "../actions/shared";
+
 class App extends React.Component {
   componentDidMount() {}
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import reducer from "./reducers";
+import middleware from "./middleware";
 
 import App from "./components/App";
 
+const store = createStore(reducer, middleware);
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
