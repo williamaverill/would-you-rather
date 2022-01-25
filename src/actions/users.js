59a4e2fbe6ce28e0b93c68f112a6bcda0decf771
Will Averill <wsaverill@gmail.com>
2022-01-24 18:47:14 -0800

    Update user actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,5 @@
 export const LOG_IN = "LOG_IN";
 export const LOG_OUT = "LOG_OUT";
-export const CREATE_USER = "CREATE_USER";
-export const CHANGE_AVATAR = "CHANGE_AVATAR";
-export const GET_SCORE = "GET_SCORE";
 
 function logIn(id) {
   return {
@@ -18,28 +15,6 @@ function logOut(id) {
   };
 }
 
-function createUser(user) {
-  return {
-    type: CREATE_USER,
-    user,
-  };
-}
-
-function changeAvatar(id, avatarURL) {
-  return {
-    type: CHANGE_AVATAR,
-    id,
-    avatarURL,
-  };
-}
-
-function getScore(id) {
-  return {
-    type: GET_SCORE,
-    id,
-  };
-}
-
 export function handleLogIn(id) {
   return (dispatch) => {
     dispatch(logIn(id));
@@ -49,23 +24,5 @@ export function handleLogIn(id) {
 export function handleLogOut(id) {
   return (dispatch) => {
     dispatch(logOut(id));
-  };
-}
-
-export function handleCreateUser(user) {
-  return (dispatch) => {
-    dispatch(createUser(user));
-  };
-}
-
-export function handleChangeAvatar(id, avatarURL) {
-  return (dispatch) => {
-    dispatch(changeAvatar(id, avatarURL));
-  };
-}
-
-export function handleGetScore(id) {
-  return (dispatch) => {
-    dispatch(getScore(id));
   };
 }
